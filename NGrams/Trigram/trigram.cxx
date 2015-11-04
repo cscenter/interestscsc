cfcf19aca4e03dab513dd7f585ccfdd614fb7348
korskovan <korskovan@gmail.com>
2015-11-04 09:41:07 +0300

    1,2,3-grams

--- NGrams/Trigram/trigram.cxx
+++
@@ -1,8 +0,0 @@
-#encoding "utf-8"    // сообщаем парсеру о том, в какой кодировке написана грамматика
-#GRAMMAR_ROOT S      // указываем корневой нетерминал грамматики
-
-Trigrams ->  Adj<gram='A', gnc-agr[1]> Adj<gram='A', gnc-agr[1]>  Noun<gnc-agr[1], rt> ;
-
-S -> Trigrams interp(NGrams.Trigram);
-
-
