22af82e2ceb019e943d88d2e106b75bd890189d0
Maxim <sssmaximusss@gmail.com>
2016-03-04 00:50:25 +0300

    Oops... delete unused changes

--- src/main/java/com/interestscsc/crawler/Main.java
+++ src/main/java/com/interestscsc/crawler/Main.java
@@ -4,7 +4,6 @@ import com.interestscsc.db.DBConnector;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
-import java.util.Calendar;
 
 public class Main {
 
@@ -12,15 +11,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Calendar calendar = Calendar.getInstance();
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int year = calendar.get(Calendar.YEAR);
-
         String startUser = "mi3ch";
         try {
-            Crawler ljCrawler = new Crawler(DBConnector.DataBase.PROD, System.getProperty("user.name") + "-" +
-                    year + "-" + month + "-" + day);
+            Crawler ljCrawler = new Crawler(DBConnector.DataBase.PROD, System.getProperty("user.name"));
             ljCrawler.crawl(startUser);
         } catch (SQLException sqle) {
             logger.error("Error working with DB. " + sqle);
